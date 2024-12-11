dd5584002588ea64b6c71653666ac772fc50d74d
drewski <drewskiwooskie@gmail.com>
2024-12-11 09:55:52 -0500

    test13

--- build_files/install_repos.sh
+++ build_files/install_repos.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 curl --retry 3 -Lo /etc/yum.repos.d/starship-fedpra/repo \
-	https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-%OS_VERSION%/atim-starship-fedora-%OS_VERSION%.repo
+	https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${FEDORA_MAJOR_VERSION}"/atim-starship-fedora-"${FEDORA_MAJOR_VERSION}".repo
 curl --retry 3 -Lo /etc/yum.repos.d/hardened-chromium.repo \
-	https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-%OS_VERSION%/secureblue-hardened-chromium-fedora-%OS_VERSION%.repo
+	https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-hardened-chromium-fedora-"${FEDORA_MAJOR_VERSION}".repo
 curl --retry 3 -Lo  /etc/yum.repos.d/bubblejail.repo \
-	https://copr.fedorainfracloud.org/coprs/secureblue/bubblejail/repo/fedora-%OS_VERSION%/secureblue-bubblejail-fedora-%OS_VERSION%.repo
+	https://copr.fedorainfracloud.org/coprs/secureblue/bubblejail/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-bubblejail-fedora-"${FEDORA_MAJOR_VERSION}".repo
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
