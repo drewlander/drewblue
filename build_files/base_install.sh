166a511ef88f59ccebf9fb77cc0c943f63b0bd71
drewski <drewskiwooskie@gmail.com>
2024-12-11 09:45:32 -0500

    test11

--- build_files/base_install.sh
+++ build_files/base_install.sh
@@ -1,5 +1,5 @@
-install_repos.sh
-base_install_packages.sh
-install_multimedia.sh
+/ctx/build_files/install_repos.sh
+/ctx/build_files/base_install_packages.sh
+/ctx/build_files/install_multimedia.sh
 
 
