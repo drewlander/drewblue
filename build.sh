3ea7adb997d521a8532258f9036e8c29eecb6883
Drewlander <drewlander@users.noreply.github.com>
2025-02-23 09:42:13 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,13 @@ RELEASE="$(rpm -E %fedora)"
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
-dnf install -y tmux neovim
+dnf install -y tmux neovim emacs
 #### Example for enabling a System Unit File
-build_files/base_install.sh
+curl --retry 3 -Lo /etc/yum.repos.d/hardened-chromium.repo \
+	https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-hardened-chromium-fedora-"${FEDORA_MAJOR_VERSION}".repo
+curl --retry 3 -Lo  /etc/yum.repos.d/bubblejail.repo \
+	https://copr.fedorainfracloud.org/coprs/secureblue/bubblejail/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-bubblejail-fedora-"${FEDORA_MAJOR_VERSION}".repo
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
 systemctl enable podman.socket
