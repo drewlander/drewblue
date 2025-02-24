795ae8afc864008c1821a6669e60f762ff5deb36
Drewlander <drewlander@users.noreply.github.com>
2025-02-23 19:54:29 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,8 +17,46 @@ RELEASE="$(rpm -E %fedora)"
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
+rpm-ostree remove firefox firefox-langpacks
 dnf install -y @virtualization
-dnf install -y tmux neovim emacs chromium zsh
+dnf install -y @development-tools
+dnf install -y tmux neovim emacs chromium zsh 
+dnf install -y distrobox\ 
+      usbguard\ 
+      usbguard-notifier\ 
+      setroubleshoot\ 
+      setools\ 
+      fscrypt\ 
+      vim\ 
+      pam-u2f\ 
+      pam_yubico\ 
+      pamu2fcfg\ 
+      yubikey-manager\ 
+      google-noto-fonts-all\ 
+      headsetcontrol\ 
+      gnome-text-editor\ 
+      evince
+flatpak install --system-y flathub org.mozilla.firefox \
+        org.signal.Signal\ 
+        com.discordapp.Discord\ 
+        im.riot.Riot\ 
+        org.telegram.desktop\ 
+        org.libreoffice.LibreOffice\ 
+        com.github.tchx84.Flatseal\ 
+        com.mattjakeman.ExtensionManager\ 
+        com.usebottles.bottles\ 
+        net.lutris.Lutris\ 
+        io.github.dvlv.boxbuddyrs\ 
+        org.gnome.Evolution\ 
+        org.videolan.VLC\ 
+        io.mpv.Mpv\ 
+        org.atheme.audacious\ 
+        org.gnome.Builder\ 
+        com.visualstudio.code\ 
+        org.xiphos.Xiphos\ 
+        org.mozilla.Thunderbird\ 
+        org.strawberrymusicplayer.strawberry\ 
+        org.mozilla.firefox
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
