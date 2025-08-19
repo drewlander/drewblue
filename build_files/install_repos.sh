1407b69c4877859111313fb7341df6d918e30a94
user <user@android-7122cf5900fceb8e.lan>
2025-08-19 12:42:55 -0400

    kde

--- build_files/install_repos.sh
+++ build_files/install_repos.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/bash
-curl --retry 3 -Lo /etc/yum.repos.d/hardened-chromium.repo \
-	https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-hardened-chromium-fedora-"${FEDORA_MAJOR_VERSION}".repo
-curl --retry 3 -Lo  /etc/yum.repos.d/bubblejail.repo \
-	https://copr.fedorainfracloud.org/coprs/secureblue/bubblejail/repo/fedora-"${FEDORA_MAJOR_VERSION}"/secureblue-bubblejail-fedora-"${FEDORA_MAJOR_VERSION}".repo
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
